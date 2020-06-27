364a283fce9e2479c4d1e0449ca5b08b72aa8d0c
YTGamer 3000 <48304290+YTGamer@users.noreply.github.com>
2020-06-27 13:49:30 +0200

    Create diep_io_dark_neon_theme.js

---
+++ other/diep_io_dark_neon_theme.js
@@ -0,0 +1,35 @@
+/*
+ * Diep.io Dark Neon Theme by lonl5445
+ * How to use it?
+ * Open diep.io and open the console using the Home button (or the "top-left arrow key")
+ * Paste the code just below
+ * And enjoy!
+ * Note 1: If you leave/reload the game, you must paste the code again.
+ * Note 2: This theme also allows you to view the FPS counter and the exact value of the health of an user.
+ */
+ren_stroke_soft_color_intensity -10;
+ren_background_color 0;
+ren_solid_background true;
+ren_fps true;
+ren_grid_color 3289650;
+ren_health_fill_color 30506;
+ren_health_background_color 855309;
+ren_minimap_background_color 3289650;
+ren_minimap_border_color 855309;
+ren_score_bar_fill_color 30506;
+ren_xp_bar_fill_color 6579200;
+ren_background_color 0;
+ren_raw_health_values true;
+net_replace_color 1 986895;
+net_replace_color 8 1315840;
+net_replace_color 9 1508870;
+net_replace_color 10 3350;
+net_replace_color 11 1443095;
+net_replace_color 12 1315840;
+net_replace_color 2 4375;
+net_replace_color 15 1310720;
+net_replace_color 3 4375;
+net_replace_color 4 1310720;
+net_replace_color 5 590867;
+net_replace_color 6 4634;
+net_replace_color 13 30506;
